b93ccb8277a634c518f0fecd17f2aead6a1d173e
Justinr2940 <78707357+Justinr2940@users.noreply.github.com>
2026-07-13 23:24:12 -0400

    Home page image change

--- app/page.tsx
+++ app/page.tsx
@@ -117,7 +117,7 @@ export default function Home() {
       <div className="overflow-hidden rounded-[2rem] bg-white h-[500px] flex items-center justify-center">
 
         <img
-          src="/icecream2.png"
+          src="/icecream2.jpeg"
           className="max-h-[500px] w-auto object-contain group-hover:scale-105 transition duration-700"
           alt="Ice Cream Sundae Bar"
         />
